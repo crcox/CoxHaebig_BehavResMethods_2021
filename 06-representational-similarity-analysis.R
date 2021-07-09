cbef83d45ea8cb35a22994843f3831b969ae16cc
Chris Cox <chriscox@lsu.edu>
2021-07-09 17:24:21 -0500

    revised to reference assoc-preproc

--- 06-representational-similarity-analysis.R
+++ 06-representational-similarity-analysis.R
@@ -11,36 +11,6 @@ trim_prefix <- function(x, prefix) {
     return(trimws(x, which = "left", whitespace = prefix))
 }
 
-count_participants_by_cue <- function(cues, participant_id) {
-  X <- lapply(
-    split(
-      as.factor(participant_id),
-      as.factor(cues),
-      drop = TRUE
-    ),
-    FUN = droplevels)
-  return(vapply(X = X, FUN = nlevels, FUN.VALUE = numeric(1)))
-}
-
-sample_participants_by_cue <- function(d, n) {
-    tidyr::pivot_wider(
-        data = d,
-        id = tidyselect::everything(),
-        names_prefix = "R",
-        names_from = "RESP_ID",
-        values_from = "RESPONSE"
-    ) %>%
-    dplyr::group_by(CUE) %>%
-    dplyr::slice_sample(n = n) %>%
-    dplyr::ungroup() %>%
-    tidyr::pivot_longer(
-        cols = c("R1", "R2", "R3"),
-        names_to = "RESP_ID",
-        names_prefix = "R",
-        values_to = "RESPONSE"
-    )
-}
-
 generate_response_profiles <- function(d) {
     xtabs(~ RESPONSE + CUE, data = d)
 }
@@ -127,37 +97,8 @@ simulate_null_cor <- function(wide_assoc) {
 }
 
 # Load association data ----
-associations <- load_to_list(c("./data/associations-adult.Rdata", "./data/associations-child.Rdata"))
+associations <- load_to_list(c("./data/associations-adult-preproc.Rdata", "./data/associations-child-preproc.Rdata"))
 names(associations) <- trim_prefix(names(associations), "associations_")
-associations$adult$RESPONSE <- as.character(associations$adult$RESPONSE)
-associations$child$RESPONSE <- as.character(associations$child$RESPONSE)
-associations$adult$CUE <- as.character(associations$adult$CUE)
-associations$child$CUE <- as.character(associations$child$CUE)
-associations$adult$PP_ID <- as.character(associations$adult$PP_ID)
-associations$child$PP_ID <- as.character(associations$child$PP_ID)
-
-associations <- lapply(
-    associations,
-    function (d) {
-        droplevels(subset(d, CUE !="so big!"))
-    }
-)
-
-stopifnot(all.equal(
-    levels(associations[['adult']]['CUE']),
-    levels(associations[['child']]['CUE'])
-))
-
-# Check that at least 95 participants responded to all retained cues ----
-pp_count <- vapply(
-  associations,
-  FUN = function(d) count_participants_by_cue(d$CUE, d$PP_ID),
-  FUN.VALUE = numeric(length(unique(associations$adult$CUE)))
-)
-stopifnot(all(pmin(pp_count[, 1], pp_count[, 2]) > 95))
-
-# Select 100 participants (300 responses) per cue ----
-associations <- lapply(associations, sample_participants_by_cue, n = 100)
 
 # Compute true representational similarity matrices ----
 repsim <- lapply(
@@ -175,7 +116,7 @@ repsim_cor <- cor(repsim$adult, repsim$child, method = "spearman")
 library('parallel')
 cl <- parallel::makeCluster(parallel::detectCores() - 1)
 parallel::clusterSetRNGStream(cl)
-parallel::invisible(clusterEvalQ(cl, {
+invisible(parallel::clusterEvalQ(cl, {
   library('tidyr')
 }))
 clusterExport(cl, c(
